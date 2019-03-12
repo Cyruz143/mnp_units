40ff59085c006c5fdeacb1c583de104bdd9900fc
Cyruz143 <root.cyruz@gmail.com>
2019-03-12 08:49:32 +0000

    Correct icon paths

--- addons/mnp_blufor/CfgFactionClasses.h
+++ addons/mnp_blufor/CfgFactionClasses.h
@@ -2,98 +2,98 @@ class CfgFactionClasses {
     class MNP_US_RAN {
         displayName = "United States Rangers";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 2;
         side = 1;
     };
     class MNP_CA_WD {
         displayName = "Canadian Army Woodland";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 5;
         side = 1;
     };
     class MNP_CA_DE {
         displayName = "Canadian Army Desert";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 5;
         side = 1;
     };
     class MNP_US_Sco {
         displayName = "United States Army Scorpion";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 3;
         side = 1;
     };
     class MNP_GE_WD {
         displayName = "German Bundeswehr";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 4;
         side = 1;
     };
     class MNP_GE_DE {
         displayName = "German Bundeswehr Desert";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 4;
         side = 1;
     };
     class MNP_AUS {
         displayName = "Australian Army (Auscam)";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 6;
         side = 1;
     };
     class MNP_ROK {
         displayName = "Republic of Korea";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 7;
         side = 1;
     };
     class MNP_USMCA_SN {
         displayName = "USMC Snow";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 1;
         side = 1;
     };
     class MNP_UKR {
         displayName = "Ukrainian Armed Forces";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 4;
         side = 1;
     };
     class MNP_ROKMC {
         displayName = "Republic of Korea Marines";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 7;
         side = 1;
     };
     class MNP_ISR {
         displayName = "Israeli Defence Forces";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 3;
         side = 1;
     };
     class MNP_AMCU {
         displayName = "Australian Multicam";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 6;
         side = 1;
     };
     class MNP_Irregulars {
         displayName = "Desert Irregulars";
         author = "Killoch";
-        icon = "\MNP_BLUFOR\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 3;
         side = 1;
     };
--- addons/mnp_indep/CfgFactionClasses.h
+++ addons/mnp_indep/CfgFactionClasses.h
@@ -2,63 +2,63 @@ class CfgFactionClasses {
     class MNP_MIL_WD {
         displayName = "Local Volunteer Militia Woodland";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 1;
         side = 3;
     };
     class MNP_MIL_DE {
         displayName = "Local Volunteer Militia Desert";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 1;
         side = 3;
     };
     class MNP_IRE_WD {
         displayName = "Defence Forces Ireland Woodland";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 5;
         side = 3;
     };
     class MNP_IRE_DE {
         displayName = "Defence Forces Ireland Desert";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 5;
         side = 3;
     };
     class MNP_NZ {
         displayName = "New Zealand Defence Forces";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 6;
         side = 3;
     };
     class MNP_FIN_WD {
         displayName = "Finish Defence Forces Woodland";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 7;
         side = 3;
     };
     class MNP_FIN_SN {
         displayName = "Finish Defence Forces Snow";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 7;
         side = 3;
     };
     class MNP_NOR_WD {
         displayName = "Norwegian Army Woodland";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 8;
         side = 3;
     };
     class MNP_NOR_DE {
         displayName = "Norwegian Army Desert";
         author = "Killoch";
-        icon = "\MNP_INDEP\Data\Icon.paa";
+        icon = "\x\mnp_units\addons\mnp_main\data\icon.paa";
         priority = 8;
         side = 3;
     };
